bef1934b0b7e249b3dc610fa042509cda95767e8
Endermaiter <mpereznovoa@danielcastelao.org>
2023-11-15 21:42:46 +0100

    Version mejorada del ejercicio de keyla :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        do {
+            Garaje gar = new Garaje();
+            gar.sitio();
+        }while (true);
     }
 }
